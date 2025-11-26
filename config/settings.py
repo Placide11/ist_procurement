9de1c13095ced394595a77a4aea2cb4cea7d87de
PSHEMA <p.shema@alustudent.com>
2025-11-26 21:51:00 +0200

    Debug

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY ='django-insecure-(4fuvkh0q8gs!j8f_4^*&#k%k6i-gkehujg6j_(o1=mo3e41mm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "ist-procurement.onrender.com",
